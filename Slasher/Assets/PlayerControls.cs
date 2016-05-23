3721855678cd477cdcc4e4c501f0ed52a892327b
Remi <r.androuin@student.isartdigital.com>
2016-05-23 16:04:19 +0200

    Added character moves and wall

---
+++ Slasher/Assets/PlayerControls.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float xOffset = Input.GetAxis("Horizontal");
+		float yOffset = Input.GetAxis("Vertical");
+		Vector2 newPos = new Vector2(transform.position.x + xOffset, transform.position.y + yOffset);
+		rb.MovePosition(newPos);
+	}
+}
